73f8c66273677a60242e9b85b34160189688d377
ikedakoichiro13 <git@github.com:ikedakoichiro13/Bookers.git>
2024-05-22 14:05:43 +0000

    booksers2-2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :user, only: [:show, :edit]
+  resources :book, only: [:index, :new, :show]
   root to: 'homes#top'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
