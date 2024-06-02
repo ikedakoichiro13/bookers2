392f4a4e320687a8ba426b8f28e394e6a4da2bd9
ikedakoichiro13 <git@github.com:ikedakoichiro13/Bookers.git>
2024-06-02 13:24:21 +0000

    bookers2-4

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,4 +3,31 @@ class BooksController < ApplicationController
   def new
     @books = Book.new
   end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+
+  def index
+     @users = User.all
+     @user = current_user
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,37 @@
 class UsersController < ApplicationController
+
   def index
+    @users = User.all
+    @user = current_user
   end
+
   def show
+    @user = current_user
   end
 
   def edit
+    @user = current_user
   end
 
+  def create
+    @user = User.new(user_params)
+    @user.user_id = current_user.id
+    @user.save
+    redirect_to books_path
+ end
+
   def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      render '/users/:id/edit'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
   end
 end
